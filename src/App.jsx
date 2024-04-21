f089719a9a91352c5444ae78e724b31b7d8050e6
Gary Lloyd M. Senoc <62071560+garysenoc@users.noreply.github.com>
2024-04-21 21:34:32 +0800

    Fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { useState, useEffect } from 'react';
 import './App.css';
-import {get, set} from "local-storage";
 import Swal from "sweetalert2";
 import { useNavigate  } from "react-router-dom";
 
@@ -11,26 +10,26 @@ function App() {
   const navigate = useNavigate();
   
   useEffect(() => {
-    const xWins = get('xWins');
-    const oWins = get('oWins');
-    const draw = get('draw');
+    const xWins = localStorage.getItem('xWins');
+    const oWins = localStorage.getItem('oWins');
+    const draw = localStorage.getItem('draw');
 
     // Initialize local storage values if they are missing
     if (xWins === null || oWins === null || draw === null) {
-      set('xWins', 0);
-      set('oWins', 0);
-      set('draw', 0);
+      localStorage. localStorage.setItem('xWins', 0);
+      localStorage.setItem('oWins', 0);
+      localStorage.setItem('draw', 0);
     }
   }, []);
 
   const createHistory = async () => {
     const body = {
-      xWins: get('xWins') || 0,
-      oWins: get('oWins') || 0,
-      draw: get('draw') || 0,
+      xWins:  localStorage.getItem('xWins') || 0,
+      oWins: localStorage.getItem('oWins') || 0,
+      draw: localStorage.getItem('draw') || 0,
       dateCreated: new Date().toISOString(),
-      xPlayerName: get('playerX') || '',
-      oPlayerName: get('playerO') || '',
+      xPlayerName: localStorage.getItem('playerX') || '',
+      oPlayerName: localStorage.getItem('playerO') || '',
     };
   
     try {
@@ -131,17 +130,17 @@ function App() {
     if (gameWinner) {
       setWinner(gameWinner);
       if (gameWinner === 'X') {
-        const getXwins = get('xWins');
-        set('xWins', getXwins + 1);
+        const getXwins = localStorage.getItem('xWins');
+        localStorage.setItem('xWins', getXwins + 1);
       } else if (gameWinner === 'O') {
-        const getOwins = get('oWins');
-        set('oWins', getOwins + 1);
+        const getOwins = localStorage.getItem('oWins');
+        localStorage.setItem('oWins', getOwins + 1);
       }
 
       continueStopModal(gameWinner);
     } else if (newBoard.every((square) => square)) {
-      const getDraw = get('draw');
-      set('draw', getDraw + 1);
+      const getDraw = localStorage.getItem('draw');
+      localStorage.setItem('draw', getDraw + 1);
 
       continueStopModal('Draw');
     }
@@ -181,9 +180,6 @@ function App() {
       </div>
       {winner && <h2>{winner} wins!</h2>}
       {renderDrawMessage()}
-      <button className="reset-btn" onClick={resetGame}>
-        Reset Game {get('playerX')}
-      </button>
     </div>
   );
 }
